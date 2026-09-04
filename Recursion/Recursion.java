59dff6c2d6d4fe3b2b8b72b9a8c89863d27a064b
Dev kumar <devkmandal0@gmail.com>
2026-09-04 12:36:00 +0530

    Print summation of N number using recursion

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,10 +1,16 @@
 void main() {
+    System.out.println();
 
-printNumber(1,10);
 
+//printNumber(1,10);
+
+    sumOfFirstNnumber(10,0);
+    System.out.println(sumOfNNumber(10,0));
 
 
 }
+
+
 
 
 void printNumber (int i,int n){
@@ -14,4 +20,26 @@ if(i>10){
 
     System.out.println(i);
     printNumber(i+1,n);
+}
+
+
+// Using parametrized.
+void sumOfFirstNnumber(int i,int sum){
+    if(i < 1){
+        System.out.println(sum);
+        return;
+    }
+
+    sumOfFirstNnumber(i-1,sum+i);
+
+}
+
+
+// using function return.
+int sumOfNNumber(int i,int sum){
+    if(i<1){
+        return  sum+i;
+    }
+    sum = sumOfNNumber(i-1,sum);
+    return  sum+i;
 }
