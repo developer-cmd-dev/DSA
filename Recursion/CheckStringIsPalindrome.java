7c57e19831f31f5081dacf19c109fddfe579e053
Dev kumar <devkmandal0@gmail.com>
2026-09-04 13:29:15 +0530

    Solved CheckPalindrome using recrusion

--- Recursion/CheckStringIsPalindrome.java
+++ Recursion/CheckStringIsPalindrome.java
@@ -1,3 +1,24 @@
 void main() {
-  $END$
+
+    String str = "Madam";
+
+    str = str.toUpperCase();
+
+   char[] chars = str.toCharArray();
+
+    System.out.println(checkPalindrome(0,chars.length,chars));
+
+
+
+
+}
+
+
+
+boolean checkPalindrome(int i,int n,char[] s){
+    if(i>=n/2){
+        return true;
+    }
+    if(s[i]!=s[n-i-1])return false;
+     else return checkPalindrome(i+1,n,s);
 }
